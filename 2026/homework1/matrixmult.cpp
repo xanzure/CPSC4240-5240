b212d0ba1cef7019a3ae535d3bdcdfdf54310a04
Xander Slater <xandersl8r@gmail.com>
2026-03-01 21:52:06 -0500

    Solution

--- 2026/homework1/matrixmult.cpp
+++ 2026/homework1/matrixmult.cpp
@@ -4,21 +4,26 @@
  * Multiplies two square matrices of size n x n.
  *******************************************************/
 
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 #include <omp.h>
+
 
 #include "parlaylib/include/parlay/primitives.h"
 #include "parlaylib/include/parlay/parallel.h"
 #include "parlaylib/include/parlay/sequence.h"
 #include "parlaylib/include/parlay/utilities.h"
 
+
 using namespace std;
+
 
 int main() {
     int n;
     std::cin >> n;
+
 
     // Create matrices A, B, and C (all n x n)
     std::vector<std::vector<int>> A(n, std::vector<int>(n));
@@ -28,12 +33,14 @@ int main() {
     std::vector<std::vector<int>> E(n, std::vector<int>(n));
     std::vector<std::vector<int>> F(n, std::vector<int>(n, 0));
 
+
     // Read matrix A
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             std::cin >> A[i][j];
         }
     }
+
 
     // Read matrix B
     for (int i = 0; i < n; ++i) {
@@ -42,12 +49,14 @@ int main() {
         }
     }
 
+
     // Read matrix D
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             std::cin >> D[i][j];
         }
     }
+
 
     // Read matrix E
     for (int i = 0; i < n; ++i) {
@@ -56,11 +65,37 @@ int main() {
         }
     }
 
+
     auto startC = chrono::high_resolution_clock::now();
     // TODO (OpenMP): perform matrix multiplication A x B and write into C: C = A x B
     // YOUR OpenMP CODE HERE
+    /*#pragma omp parallel for
+    for(int i = 0; i < n; i ++)
+    {
+        for(int k = 0; k < n; k++)
+        {
+            for(int j = 0; j < ; j++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }*/
+
+    //Sequential Solution
+    for(int i = 0; i < n; i ++)
+    {
+        for(int k = 0; k < n; k++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
     auto endC = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsedC = endC - startC;
+
 
     std::cout << "The resulting matrix C = A x B is:\n";
     for (int i = 0; i < n; ++i) {
@@ -70,11 +105,49 @@ int main() {
         std::cout << "\n";
     }
 
+
     auto startF = chrono::high_resolution_clock::now();
     // TODO (ParlayLib): perform matrix multiplication D x E and write into F: F = D x E
     // YOUR ParlayLib CODE HERE
+    /*parlay::parallel_for(0, n, [&](int i) {
+        for (int k = 0; k < n; k++)
+        {
+            for (int j = 0; j < n; j++) {
+                F[i][j] += D[i][k] * E[k][j];
+            }
+        }
+    });*/
+
+    //Parallel and Blocking Solution
+    int BS = 32; //block size
+    parlay::parallel_for(0, (n + BS-1), [&](int bi) {
+        int ii = bi * BS;
+        for(int kk = 0; kk < n; kk += BS)
+        {
+            for(int jj = 0; jj < n; jj += BS)
+            {
+                int i_max = min(ii + BS, n);
+                int k_max = min(kk + BS, n);
+                int j_max = min(jj + BS, n);
+
+                for(int i = ii; i < i_max; i++)
+                {
+                    for(int k = kk; k < k_max; k++)
+                    {
+                        int dik = D[i][k];
+                        for(int j = jj; j < j_max; j++)
+                        {
+                            F[i][j] += dik * E[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    });
+
     auto endF = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsedF = endF - startF;
+
 
     std::cout << "The resulting matrix F = D x E is:\n";
     for (int i = 0; i < n; ++i) {
@@ -84,8 +157,12 @@ int main() {
         std::cout << "\n";
     }
 
+
     cout << "TIME_C:" << elapsedC.count() << endl;
     cout << "TIME_F:" << elapsedF.count() << endl;
 
     return 0;
 }
+
+
+
